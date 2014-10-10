12ff25e67540655ef28611b6cbf2f86a55753e74
Martika <martikaschwan@yahoo.de>
2014-10-10 21:28:01 +0200

    Bugfix
    Change-Id: Iec6017b9baec12e36f671ec476d8308f948ae865
    Signed-off-by: Martika <martikaschwan@yahoo.de>

--- src/Uhrendarstellung.java
+++ src/Uhrendarstellung.java
@@ -32,39 +32,39 @@ public class Uhrendarstellung extends JPanel{
     g.setColor(Color.white);
     g.setFont( new Font( "Arial", Font.BOLD, 23 ) );
     g.drawString("B",35,60);
-    g.drawString("i",35,85);
-    g.drawString("t",35,110);
-    g.drawString("t",35,135);
-    g.drawString("e",33,160);
+    g.drawString("i",39,85);
+    g.drawString("t",38,110);
+    g.drawString("t",38,135);
+    g.drawString("e",36,160);
 
     g.drawString("U",35,200);
-    g.drawString("h",35,225);
-    g.drawString("r",35,250);
+    g.drawString("h",36,225);
+    g.drawString("r",38,250);
 
     g.drawString("W",35,290);
-    g.drawString("ä",35,315);
-    g.drawString("h",35,340);
-    g.drawString("l",35,365);
-    g.drawString("e",35,390);
-    g.drawString("n",35,415);
+    g.drawString("ä",36,315);
+    g.drawString("h",36,340);
+    g.drawString("l",39,365);
+    g.drawString("e",36,390);
+    g.drawString("n",36,415);
 
 
     g.drawString("B",460,60);
-    g.drawString("i",460,85);
-    g.drawString("t",460,110);
-    g.drawString("t",460,135);
-    g.drawString("e",460,160);
+    g.drawString("i",464,85);
+    g.drawString("t",463,110);
+    g.drawString("t",463,135);
+    g.drawString("e",461,160);
 
     g.drawString("U",460,200);
-    g.drawString("h",460,225);
-    g.drawString("r",460,250);
+    g.drawString("h",461,225);
+    g.drawString("r",463,250);
 
     g.drawString("W",460,290);
-    g.drawString("ä",460,315);
-    g.drawString("h",460,340);
-    g.drawString("l",460,365);
-    g.drawString("e",460,390);
-    g.drawString("n",460,415);
+    g.drawString("ä",461,315);
+    g.drawString("h",461,340);
+    g.drawString("l",465,365);
+    g.drawString("e",461,390);
+    g.drawString("n",461,415);
     
   }
 }
